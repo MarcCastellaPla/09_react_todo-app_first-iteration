12a1ba969967d33e0b9116616b04dad8cb6371fc
MarcCastellaPla <145784203+MarcCastellaPla@users.noreply.github.com>
2025-03-09 22:16:22 +0100

    fix: undo vite.config.js changes+

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import {
 } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
 })
